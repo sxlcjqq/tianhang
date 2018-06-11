73420b8603be73933f2cc01a7e8e3ec5952d9454
120674276@qq.com <120674276@qq.com>
2018-06-11 18:04:01 +0800

    'tree'

--- src/components/page/Ggsy.vue
+++ src/components/page/Ggsy.vue
@@ -10,6 +10,7 @@
             <div class="form-box">
                 <p>监管首页</p>
             </div>
+            <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" style="margin-top:20px;"></el-tree>
         </div>
 
     </div>
@@ -20,9 +21,64 @@
         name: 'jgst',
         data: function(){
             return {
+              data: [{
+                label: '一级 1',
+                children: [{
+                  label: '二级 1-1',
+                  children: [{
+                    label: '三级 1-1-1'
+                  }]
+                }]
+              }, {
+                label: '一级 2',
+                children: [{
+                  label: '二级 2-1',
+                  children: [{
+                    label: '三级 2-1-1'
+                  }]
+                }, {
+                  label: '二级 2-2',
+                  children: [{
+                    label: '三级 2-2-1'
+                  }]
+                }]
+              }, {
+                label: '一级 3',
+                children: [{
+                  label: '二级 3-1',
+                  children: [{
+                    label: '三级 3-1-1'
+                  }]
+                }, {
+                  label: '二级 3-2',
+                  children: [{
+                    label: '三级 3-2-1'
+                  }]
+                }]
+              }],
+              defaultProps: {
+                children: 'children',
+                label: 'label'
+              }
             }
         },
         methods: {
+          handleNodeClick(data) {
+            console.log(data);
+          }
         }
     }
 </script>
+<style>
+.el-tree-node__content>.el-tree-node__expand-icon{
+  background: url(../../../static/img/line.png) no-repeat center;
+  background-size:contain;
+}
+.el-tree-node__content{
+  background: url(../../../static/img/line2.png) no-repeat left center;
+  background-size:contain;
+}
+.el-tree-node__expand-icon.expanded{
+  transform:none;
+}
+</style>
